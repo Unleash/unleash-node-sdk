ffe109d01ca340777afa2aa463c0ab04de46eaed
Simon Hornby <simon@getunleash.io>
2025-11-27 15:41:11 +0200

    chore: move streaming code into repo folder (#781)
    * chore: move repository tests into repository folder
    
    * feat: bolt in failover strategy (#782)

---
+++ src/test/repository/streaming-fetcher.test.ts
@@ -0,0 +1,96 @@
+import test from 'ava';
+import { StreamingFetcher } from '../../repository/streaming-fetcher';
+import { UnleashEvents } from '../../events';
+import type { StreamingFetchingOptions } from '../../repository/fetcher';
+
+function makeOptions(overrides: Partial<StreamingFetchingOptions> = {}): StreamingFetchingOptions {
+  return {
+    url: 'https://example.com',
+    appName: 'test-app',
+    instanceId: 'test-instance',
+    headers: {},
+    connectionId: 'conn-1',
+    onSaveDelta: async () => {},
+    onModeChange: async () => {},
+    eventSource: undefined,
+    ...overrides,
+  } as any;
+}
+
+test('emits Warn on SSE when failover is triggered', async (t) => {
+  const warnings: unknown[] = [];
+  const options = makeOptions({
+    onModeChange: async () => {},
+  });
+
+  const fetcher = new StreamingFetcher(options);
+  const anyFetcher = fetcher as any;
+
+  anyFetcher.failoverStrategy.shouldFailover = () => true;
+
+  fetcher.on(UnleashEvents.Warn, (payload) => {
+    warnings.push(payload);
+  });
+
+  const error = { status: 429, message: 'Go away, there are way too many of you' };
+
+  await anyFetcher.handleErrorEvent(error);
+
+  t.is(warnings.length, 1);
+  t.is(warnings[0], 'Go away, there are way too many of you');
+});
+
+test('does not emit Warn on SSE when failover is not triggered', async (t) => {
+  const warnings: unknown[] = [];
+  const options = makeOptions({
+    onModeChange: async () => {},
+  });
+
+  const fetcher = new StreamingFetcher(options);
+  const anyFetcher = fetcher as any;
+
+  anyFetcher.failoverStrategy.shouldFailover = () => false;
+
+  fetcher.on(UnleashEvents.Warn, (payload) => {
+    warnings.push(payload);
+  });
+
+  const error = { status: 500, message: 'Temporary server issue' };
+
+  await anyFetcher.handleErrorEvent(error);
+
+  t.is(warnings.length, 0);
+});
+
+test('transient errors that cause failover report the last error', async (t) => {
+  const warnings: unknown[] = [];
+  const options = makeOptions({
+    onModeChange: async () => {},
+  });
+
+  const fetcher = new StreamingFetcher(options);
+  const anyFetcher = fetcher as any;
+
+  anyFetcher.failoverStrategy.shouldFailover = (() => {
+    const currentErrors: unknown[] = [];
+
+    return (error: unknown) => {
+      currentErrors.push(error);
+      return currentErrors.length >= 3;
+    };
+  })();
+
+  fetcher.on(UnleashEvents.Warn, (payload) => {
+    warnings.push(payload);
+  });
+
+  await anyFetcher.handleErrorEvent({ status: 500, message: 'Once in a lifetime issue' });
+  await anyFetcher.handleErrorEvent({ status: 500, message: 'Second once in a lifetime issue' });
+  await anyFetcher.handleErrorEvent({
+    status: 500,
+    message: 'Third once in a lifetime issue, thats too many',
+  });
+
+  t.is(warnings.length, 1);
+  t.is(warnings[0], 'Third once in a lifetime issue, thats too many');
+});
