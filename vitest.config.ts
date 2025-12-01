278eb4221d9e7afeccd5816c3b00d05a291f2328
Christopher Kolstad <chriswk@getunleash.io>
2025-12-01 11:13:49 +0100

    also report coverage on test failures

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: [['lcov', { projectRoot: 'src' }], ['json'], ['json-summary']],
+      reportOnFailure: true,
     },
     pool: 'threads',
   },
