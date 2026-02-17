26f64e468f1ca47e3e2cb223929abbcdff441bf2
Kris (K) Kula <krzychukula@gmail.com>
2026-02-17 15:11:00 +0100

    Add info about the issue that should solve this

--- src/re2js.d.ts
+++ src/re2js.d.ts
@@ -1,3 +1,8 @@
+/* 
+This is only needed until https://github.com/le0pard/re2js/issues/25 if fixed.
+For example when https://github.com/le0pard/re2js/pull/26 is merged 
+then remove this file! 
+*/
 declare module 're2js' {
   export { RE2JS } from 're2js';
 }
